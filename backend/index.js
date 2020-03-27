117acefe280446852e1ffba9e85520d46bc1e893
GPabloPGarcia <gpablopgarcia@gmail.com>
2020-03-27 14:19:07 -0300

    frontend

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (request, response) => {
-    return response.json({ msg: "Hello World!", success: true });
-});
-
-app.listen(3333);
